d857b56f65b3377ee4ed4cdd279965b287e20d52
kkrol1318 <kkrol.1325@wp.pl>
2025-01-06 16:36:14 +0100

    Create 1-3.py

---
+++ programy_6/1-3.py
@@ -0,0 +1,11 @@
+mobile = {
+"Model":"Iphone 15",
+"OS":"Apple",
+"Capacity":"128GB",
+"Display":"6.1",
+"Chip":"A16",
+"Camera":"48mp",
+"Weight":"171g"
+}
+for key,value in mobile.items():
+   print(f"{key} : {value}")
